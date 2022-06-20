bd598e2cd83d6cefacda714840849427d0d19397
kagame8732 <kagame8732@gmail.com>
2022-06-20 08:22:08 +0200

    update

---
+++ contact-app/src/components/ContactList.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ContactCard from "./ContactCard";
+
+export default function ContactList(props) {
+  const renderContactList = props.contacts.map((contact) => {
+    return <ContactCard contact={contact} />;
+  });
+  return <div className="ui celled list">{renderContactList}</div>;
+}
